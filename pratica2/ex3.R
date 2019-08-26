93c3afc283876c9f9bc74006d1f5ed0b3b98c3e4
andrekorol <anrobits@yahoo.com.br>
2019-08-26 20:11:47 -0300

    Change point's size and color

--- pratica2/ex3.R
+++ pratica2/ex3.R
@@ -6,7 +6,7 @@ y_axis <- c(4.9, 9.9, 19.8, 23.6, 31.3)
 dd <- data.frame(x_axis, y_axis)
 
 g <- ggplot(dd, aes(x=x_axis, y=y_axis)) +
-  geom_point(col = "darkgoldenrod", size = 4) +
+  geom_point(col = "darkred", size = 3) +
   geom_smooth(method = "lm") +
   labs(title = "R =  180Ω (valores experimentais)", y = "I (mA)", x = "E (V)") +
   theme(plot.title = element_text(hjust = 0.5))
